48c2a4242211d641370116553574905ed61be3f3
Aleksandr <alex19950318@gmail.com>
2024-08-03 19:49:17 +0300

    Create new models(catalog, product) and changed the catalog.html

--- src/goods/admin.py
+++ src/goods/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Categories,
+    Products,
+)
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Products, ProductsAdmin)
--- src/shop_app/views.py
+++ src/shop_app/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
-
 def home_view(request):
     return render(
         request=request,
