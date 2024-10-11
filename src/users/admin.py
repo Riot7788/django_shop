ba036b46e0d3985d4765b784a5387cf206347ad1
Aleksandr <alex19950318@gmail.com>
2024-10-11 20:25:35 +0300

    create user login form

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    User,
+)
+
+
+# class UserAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(User)
